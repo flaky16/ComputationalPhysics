ee18d842359ace7b559bfada20a8269d576ccbee
Sema Kuru <32938361+flaky16@users.noreply.github.com>
2018-10-21 12:36:21 +0100

    Update ProbSht3.py
    Added matrix multiplication function

--- ProbSht3.py
+++ ProbSht3.py
@@ -9,7 +9,20 @@ Computational Physics Prob Sheet 3
 
 import numpy as np
 
-# 1st question written in Xcode already
+
+# 1st Question:  (also in Xcode)
+def MatrixMultiplication( matrix1 , matrix2 ):
+    """ Gets matrices 1 and 2, in order, and returns their multiplication result as a matrix """
+    
+    assert matrix1.shape[1] == matrix2.shape[0] , "Matrix dimensions dont allow multiplication"
+    matrix3 = np.zeros( [matrix1.shape[0] , matrix2.shape[1]])   # Resultant matrix
+    for row in range(matrix1.shape[1]):
+        for column in range(matrix2.shape[0]):
+            sum_coln = 0
+            for element in range(matrix1.shape[0]):  
+                sum_coln +=  matrix1[row][element] * matrix2[element][column]   #Elements in A_mi and B_in summed over i's
+            matrix3[row][column] = sum_coln            # Sum gives C_mn
+    return matrix3
 
 # 2nd Question:
 def GaussJordan( matrix , vector):
@@ -36,6 +49,17 @@ def GaussJordan( matrix , vector):
 
 """ Function gives correct values, except for diagonal terms being zero, at which gives an error """
 if __name__ == "__main__":
+    matrix1 = np.array( [                             # Enter matrix 1 values here
+            [ 2, -5 , 2],
+            [ 0, -1 , 3],
+            [ -1, 6 , 1] ])
+    matrix2= np.array([                             # Enter matrix 2 values here
+            [ 0, -1 , 9 ],
+            [ 12, 3 , -2 ],
+            [ -2, 2 , 1 ] ])
+     
+    matrix3 = MatrixMultiplication(matrix1 , matrix2 )
+    print (matrix3)
     
     aMatrix = np.array( [[ 2., 1., 9.] ,
                          [-3., 0.,-2.] ,
