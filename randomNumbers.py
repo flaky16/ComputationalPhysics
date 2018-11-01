630c1f57861a3bbe3762584e07746e61008d5522
Sema Kuru <32938361+flaky16@users.noreply.github.com>
2018-11-01 17:45:11 +0000

    Generating random numbers according to pdf sinx is working

--- randomNumbers.py
+++ randomNumbers.py
@@ -18,16 +18,25 @@ import numpy as np
 random_no_list = []
 y = []
 x_min = 0
-x_max = np.pi
-for x in range(30000):
-    random_no = random.randint(int(x_min*100000) , int(x_max*100000)) *0.00001
+x_max = 1
+def sin_pdf_random(N):
+    """ generates N number of random numbers distributed according to pdf P(n) = 1/2 sin(n) """
+    return 0
+
+
+for x in range(10000):
+    #print(int(x_max*100000))
+    random_no = random.randint(int(x_min*100000) ,  int(x_max*100000)) *0.00001     # Random number accurate to 5 decimals
     random_no_list.append(random_no)
-    #y.append(1/2*np.sin(random_no))
-    y.append(2/np.pi * np.sin(random_no)**2)
+    random_sin_dist_no = np.arccos(1-2*random_no)      # Uniformly generated random number mapped to have a pdf 1/2sinx
+    random_sin_dist_no_list.append(random_sin_dist_no)      # Adding random numbers with pdf 1/2sinx to the list for hist
     
-#print(np.histogram(y , 10))
+
 pl.figure(1)
-pl.hist(y , 100)
+pl.hist(y , 100 , alpha = 0.7)
+#pl.scatter(t , 2/np.pi * np.sin(t)**2 , c='r')
+t = np.linspace(0,np.pi, 10000)
+pl.scatter(t , np.sin(t)*1550 , s = 0.5 , c='r')
 #pl.hist(random_no_list , 100)
 pl.figure(2)
 pl.hist(random_no_list , 50)
@@ -35,6 +44,7 @@ pl.hist(random_no_list , 50)
 
 
 pl.figure(3)                        # Comparison function
-t = np.linspace(0,np.pi, 100)
+t = np.linspace(-1,1, 100)
 pl.scatter(t , 2/np.pi * np.sin(t)**2 , c='r')
 pl.scatter(t , 2/np.pi * np.sin(t))
+#pl.scatter(t , np.arccos(2*t))
