c92b5d9ebf748ea15f7b35c5fc70abf3a9e19e1f
Sema Kuru <32938361+flaky16@users.noreply.github.com>
2018-11-10 22:31:39 +0000

    Everything put into functions, both transformation and rejection methods work

--- randomNumbers.py
+++ randomNumbers.py
@@ -9,42 +9,79 @@ Computational Physics Assignment Random Numbers
 import random
 import pylab as pl
 import numpy as np
-#y = []
-#for x in range(10000):
-#    y.append(random.randint(0,100000)*0.00001)
-    
-#print(np.histogram(y , 10))
-#pl.hist(y , 50)
-random_no_list = []
-y = []
-x_min = 0
-x_max = 1
+import time
+
+def uniform_random(N):
+    random_no_list = []
+    for x in range(N):
+        random_no = random.uniform(0 , 1)               # Generate uniform float between 0 and 1 by built in function
+        random_no_list.append(random_no)
+    return random_no_list                               # Return a list of N random numbers
+
 def sin_pdf_random(N):
-    """ generates N number of random numbers distributed according to pdf P(n) = 1/2 sin(n) """
-    return 0
+    """ returns a list of N random numbers according to pdf P(n) = 1/2 sin(n) between 0 and pi using transformation method """
+    sin_pdf = []
+    for x in range(N):
+        random_no = random.uniform(0 , 1)               # Uniform random number between 0 , 1 
+        sin_random = np.arccos(1-2*random_no)           # Mapping uniformly generated random number to have desired pdf
+                                                        # Domain becomes [0,pi]
+        sin_pdf.append(sin_random)
+    return sin_pdf                                      # Return a list of N random numbers
+
+def sin2_pdf_random(N):
+    """ returns a list of N random numbers according to pdf P(n) = 2/pi sin**2(n) between 0 and pi using rejection method """
+    accepted = []
+    while len(accepted) < N+1:                                  # Loop until N random numbers with desired pdf are generated
+        random_no = random.uniform(0 , 1)
+        random_no= np.arccos(1-2*random_no)                     # Random number with distribution of normalized comparison function
+        random_y = random.uniform(0 ,  comparison(random_no))   # Generating random y between zero and comparison function
+        if random_y < 2/np.pi *np.sin(random_no)**2:            # Check if random y is in the area enclosed by desired pdf function
+            accepted.append(random_no)
+    return accepted                                             # Return a list of N random numbers
+         
+def comparison(random_no):
+    return 2/np.pi * np.sin(random_no)                  # Comparison function for rejection method
 
 
-for x in range(10000):
-    #print(int(x_max*100000))
-    random_no = random.randint(int(x_min*100000) ,  int(x_max*100000)) *0.00001     # Random number accurate to 5 decimals
-    random_no_list.append(random_no)
-    random_sin_dist_no = np.arccos(1-2*random_no)      # Uniformly generated random number mapped to have a pdf 1/2sinx
-    random_sin_dist_no_list.append(random_sin_dist_no)      # Adding random numbers with pdf 1/2sinx to the list for hist
+if __name__ == "__main__":
+
+    N = 10**5
+    random_no_list = uniform_random(N)
     
-
-pl.figure(1)
-pl.hist(y , 100 , alpha = 0.7)
-#pl.scatter(t , 2/np.pi * np.sin(t)**2 , c='r')
-t = np.linspace(0,np.pi, 10000)
-pl.scatter(t , np.sin(t)*1550 , s = 0.5 , c='r')
-#pl.hist(random_no_list , 100)
-pl.figure(2)
-pl.hist(random_no_list , 50)
-
-
-
-pl.figure(3)                        # Comparison function
-t = np.linspace(-1,1, 100)
-pl.scatter(t , 2/np.pi * np.sin(t)**2 , c='r')
-pl.scatter(t , 2/np.pi * np.sin(t))
-#pl.scatter(t , np.arccos(2*t))
+    start_sin = time.time()
+    sin_pdf = sin_pdf_random(N)
+    end_sin = time.time()
+    
+    start_sin2 = time.time()
+    sin2_pdf = sin2_pdf_random(N)
+    end_sin2 = time.time()
+    
+    print("Time it takes for pdf = 1/2sin(x): " , end_sin - start_sin)
+    print("Time it takes for pdf = 2/pi * sin**2(x): " , end_sin2 - start_sin2)
+    print("Time ratio t2/t1 = " , (end_sin2 - start_sin2) / (end_sin - start_sin))
+    
+    t = np.linspace(0,np.pi, 1000)      # Domain for plotting the functions
+    
+    pl.figure(1)                        # Uniform distribution
+    pl.hist(random_no_list , 50)
+    pl.xlabel("x")
+    pl.ylabel("number of samples")
+    
+    
+    pl.figure(2)                        # Pdf = 1/2sin(x) distribution 
+    pl.hist(sin_pdf , 100 , alpha = 0.5)
+    pl.scatter(t , np.sin(t)*1550 , s = 0.5 , c='r')
+    pl.xlabel("x")
+    pl.ylabel("number of samples")
+    
+    pl.figure(3)                        # Pdf = 2/pi sin**2(x) distribution
+    pl.hist(sin2_pdf , 100 , alpha = 0.7)
+    pl.scatter(t , np.sin(t)**2*1950 , s = 0.5 , c='r')
+    pl.scatter(t , np.sin(t)*1950 , s = 0.5 , c='r')
+    pl.xlabel("x")
+    pl.ylabel("number of samples")
+    
+    pl.figure(4)                        # Comparison function
+    pl.scatter(t , 2/np.pi * np.sin(t)**2 , c='r')
+    pl.scatter(t , 2/np.pi * np.sin(t))
+    
